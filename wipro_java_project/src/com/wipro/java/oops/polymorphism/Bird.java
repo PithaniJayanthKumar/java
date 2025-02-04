24b581adb2936f9bd1042427d914b4d0f6ed6657
PithaniJayanthKumar <pithanijayanthkumar@gmail.com>
2025-02-04 10:57:47 +0530

    day3

---
+++ wipro_java_project/src/com/wipro/java/oops/polymorphism/Bird.java
@@ -0,0 +1,24 @@
+/**
+ * 
+ */
+package com.wipro.java.oops.polymorphism;
+
+//Parent Class
+public class Bird {
+
+	/**
+	 * 
+	 */
+	public void makeSound() {
+		System.out.println("Bird is making a sound");
+	}
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
