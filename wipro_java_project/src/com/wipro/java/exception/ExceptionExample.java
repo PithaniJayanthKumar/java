5a96035bd97acc5a4fa47d2ae04a5dc9e67168af
PithaniJayanthKumar <pithanijayanthkumar@gmail.com>
2025-02-04 15:04:17 +0530

    Day 3

---
+++ wipro_java_project/src/com/wipro/java/exception/ExceptionExample.java
@@ -0,0 +1,36 @@
+/**
+ * 
+ */
+package com.wipro.java.exception;
+
+/**
+ * 
+ */
+public class ExceptionExample {
+	
+	//it is accessible by all the instances of a class
+	//memory or storage is allocated once and remains unless it is empty
+	static  int i = 6;//it has class level declaration
+	/**
+	 * @param args
+	 */
+	public static void main(String a[]) {
+		// TODO Auto-generated method stub
+		int i=9, j=0, data;
+		try {
+			data = i/j;
+		}
+		catch(Exception e) {
+			System.out.println(e.toString());
+			
+		}
+		//block that acts as a flag to check where we are presently executing
+		//it will always executed
+		finally {
+			System.out.println("I am always Executed");
+		}
+		//from here onwards the remaining code will be executes
+		System.out.println("rest of the code");
+	}	
+
+}
