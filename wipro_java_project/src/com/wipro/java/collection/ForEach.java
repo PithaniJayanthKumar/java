775569cdb6e293937c78d475d233dd25d6d14bc2
PithaniJayanthKumar <pithanijayanthkumar@gmail.com>
2025-02-07 12:04:05 +0530

    Day 5

---
+++ wipro_java_project/src/com/wipro/java/collection/ForEach.java
@@ -0,0 +1,24 @@
+package com.wipro.java.collection;
+
+public class ForEach {
+
+	public ForEach() {
+		// TODO Auto-generated constructor stub
+	}
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		int arr[] = {1, 2, 3, 4, 5};
+		
+		//using for-each loop 
+		//introduced in java 5
+		//initialization not required, start form 0 till end
+		//condition not required 
+		//implementation not required
+		
+		for(int e : arr);
+		System.out.println(arr);
+	}
+
+}
